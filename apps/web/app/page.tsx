78eca9de71971d6394ca5267ff3eb498c770667e
venumadhav17 <kalyanamvenumadhav@gmail.com>
2025-05-26 15:06:10 +0530

    added input text in ui using typescript

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,27 @@
+import { TextInput } from "@repo/ui/text-input";
+
 export default function Home() {
-  return <div style={}></div>;
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        background: "black",
+        display: "flex",
+        justifyContent: "center",
+        justifyItems: "center"
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center"
+        }}
+      >
+        <TextInput placeholder='Room name' size='big'></TextInput>
+        <button>Join room</button>
+      </div>
+    </div>
+  );
 }
---
+++ packages/ui/src/text-input.tsx
@@ -0,0 +1,20 @@
+interface PropType {
+  placeholder: string;
+  size: "big" | "small";
+}
+
+export function TextInput({ placeholder, size }: PropType) {
+  return (
+    <div>
+      <input
+        placeholder={placeholder}
+        style={{
+          padding: size === "big" ? 20 : 10,
+          margin: size === "big" ? 20 : 10,
+          borderColor: "black",
+          borderWidth: 1
+        }}
+      ></input>
+    </div>
+  );
+}
